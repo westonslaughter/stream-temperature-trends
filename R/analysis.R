dd57d96ba4a2b6ea845d449374e995eb071bcf1c
Weston Slaughter <westonslaughter@gmail.com>
2022-04-29 13:33:15 -0400

    analsyis empty

---
+++ R/analysis.R
@@ -0,0 +1,15 @@
+### ---------------------------
+##
+## Title: helpers.R
+##
+## Purpose: this script performs basic analysis of stream temperature data
+##
+## Author: WS
+##
+## Date Created: 2022-04-29
+##
+## ---------------------------
+
+# are mean annual temperatures increasin?
+# are min annual temperatures increasin?
+# are max annual temperatures increasin?
