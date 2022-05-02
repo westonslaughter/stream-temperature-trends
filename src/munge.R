f54ed8a579031703d4735e98364bba21563b99e1
Weston Slaughter <westonslaughter@gmail.com>
2022-05-02 12:23:18 -0400

    means update

--- src/munge.R
+++ src/munge.R
@@ -11,18 +11,12 @@
 ## ---------------------------
 library(dplyr)
 library(feather)
-
 source("./src/helpers.R")
 
 ## Data
 # USGS stream temp and discharge
 raw_stream_temp <- read.csv("./data/dv/raw_temp_dv_compiled.csv")
 raw_stream_disch <- read.csv("./data/dv/raw_disch_dv_compiled.csv")
-
-# NOAA to USGS site code key
-usgs_noaa_key <- read_feather("./data/air/air_temp/usgs_noaa_key.feather")
-
-# TODO: implement multiprocessing ('featherCompiler' takes a long time)
 daymet_files <- list.files("./data/air/air_temp/daymet", full.names = TRUE)
 daymet_data <- featherCompiler(daymet_files)
 
@@ -74,52 +68,40 @@ daymet_data <- daymet_data %>%
 stream_long <- rbind(stream_temp, stream_disch)
 temp_long <- rbind(stream_long, daymet_data)
 
-# annual
-all <- temp_long %>%
-  tidyr::pivot_wider(
-           id_cols = c('date', 'site_code'),
-           names_from = var,
-           values_from = val,
-           )
-
-# interpolation?
-timer <- function(x,y){
-  data.frame(
-    date = seq(as.POSIXlt(x,format="%Y-%m-%d"),as.POSIXlt(y,format="%Y-%m-%d"),by="day")
-  )
-}
-
-## ts_daymet <- timer("1980-01-01","2022-03-28")
-
-## all_ts_interp <- merge(ts_daymet, all.1980, by="date", all=TRUE)
-## boo <- all_ts %>%
-##   mutate(across(
-##     wtr_temp:vp..Pa., ~ zoo::na.approx(.x)
-##   ))
-
-# daymet starts in 1980, so, cut to 1980
-all.1980 <- all %>%
-  filter(date > as.Date("1980-01-01"))
-;
-annual.df <- all.1980 %>%
-  tidyr::drop_na() %>%
-  mutate(year = format(as.Date(date), "%Y")) %>%
-  select(-date) %>%
-  group_by(site_code, year) %>%
-  summarise(
-    across(wtr_temp:vp..Pa., mean, .names = "mean_{.col}"),
-    across(wtr_temp:vp..Pa., min, .names = "min_{.col}"),
-    across(wtr_temp:vp..Pa., max, .names = "max_{.col}")
-  )
-
-## write_feather(annual.df, "./data/summary/annual_temp.feather")
-
 # calculate z-scores
 zcalc <- function(x) {
   (x - mean(x))/sd(x)
 }
 
 
+# annual
+annual.df <- temp_long %>%
+  tidyr::pivot_wider(
+           id_cols = c('date', 'site_code'),
+           names_from = var,
+           values_from = val,
+           ) %>%
+  # daymet starts in 1980, so, cut to 1980
+  tidyr::drop_na() %>%
+  filter(date > as.Date("1980-01-01")) %>%
+  mutate(
+    year = format(as.Date(date), "%Y"),
+    mean_air_temp = ((tmax..deg.c. + tmin..deg.c.)/2),
+    wtr_air_ratio = wtr_temp/mean_air_temp,
+    ) %>%
+  select(-date) %>%
+  group_by(site_code, year) %>%
+  summarise(
+    across(wtr_temp:wtr_air_ratio, mean, .names = "mean_{.col}"),
+    across(wtr_temp:wtr_air_ratio, min, .names = "min_{.col}"),
+    across(wtr_temp:wtr_air_ratio, max, .names = "max_{.col}")
+  )
+
 annual.df.z <- annual.df %>%
   tidyr::drop_na() %>%
-  mutate(across(mean_wtr_temp:max_vp..Pa., zcalc, .names = "z_{.col}"))
+  mutate(
+    across(
+      mean_wtr_temp:max_wtr_air_ratio, zcalc, .names = "z_{.col}")
+  )
+
+write_feather(annual.df.z, "./data/summary/annual_temp_z.feather")
